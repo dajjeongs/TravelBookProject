ddf40314fefe3b3e05873f92d2632153a75240a8
dajeong <lovehappy765@gmail.com>
2023-08-29 20:24:11 +0900

    여행명 입력 후 저장

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:travel_book/save_travel/saveTravelPage.dart';
+import 'package:travel_book/save_travel/travelPeopleNumber.dart';
+import 'package:travel_book/save_travel/travelPeriod.dart';
 import 'package:travel_book/travel_list.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:travel_book/travel_sql.dart';
+
+import 'test.dart';
 
 
 void main() => runApp(MyApp());
@@ -32,8 +36,18 @@ class TravelCashBook extends StatefulWidget {
 
 class _TravelCashBook extends State<TravelCashBook> {
 
+  final DatabaseService _databaseService = DatabaseService();
+  Future<List<Travel>> _travelList = DatabaseService()
+      .databaseConfig()
+      .then((_) => DatabaseService().selectTravels());
+
+  int currentCount = 0;
+
+
   @override
   Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height * 0.6;
 
     return Scaffold(
       appBar: AppBar(
@@ -69,8 +83,55 @@ class _TravelCashBook extends State<TravelCashBook> {
               ),
               Column(
                 children: [
-                  travelList()
-                ],
+                Container(
+                  height: height,
+                  decoration: const BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                    ),
+                  ),
+                  child:
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ScrollConfiguration(
+                        behavior: MyBehavior(),
+                        child: FutureBuilder(
+                          future: _travelList,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              currentCount = snapshot.data!.length;
+                              if (currentCount == 0) {
+                                return const Center(
+                                  child: Text("No data exists."),
+                                );
+                              } else {
+                                return ListView.builder(
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder: (context, index) {
+                                    return travelBox(
+                                        snapshot.data![index].id,
+                                        snapshot.data![index].name);
+                                    },
+                                );
+                              }
+                            } else if (snapshot.hasError) {
+                              return const Center(
+                                child: Text("Error."),
+                              );
+                            } else {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                ),
+                            );
+                          }
+                        },
+                      )
+                    ),
+                  ),
+                )
+                ]
               ),
               const SizedBox(
                 height: 30,
@@ -80,13 +141,7 @@ class _TravelCashBook extends State<TravelCashBook> {
                 children: [
                   OutlinedButton(
                       onPressed: () {
-                        setState(() {
-                          Navigator.push(context,
-                              MaterialPageRoute(
-                                  builder: (context) => SaveTravelPage()
-                              )
-                          );
-                        });
+                        addTravelBottomSheet();
                       },
                       style: OutlinedButton.styleFrom(
                           foregroundColor: Colors.black54,
@@ -121,4 +176,187 @@ class _TravelCashBook extends State<TravelCashBook> {
       ),
     );
   }
+
+  Widget travelBox(int id, String name) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(15),
+          child: Text("$id"),
+        ),
+        Container(
+          padding: const EdgeInsets.all(15),
+          child: Text(name),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // updateButton(id),
+              const SizedBox(width: 10),
+              deleteButton(id),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget deleteButton(int id) {
+    return ElevatedButton(
+        onPressed: () => showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => deleteWordDialog(id),
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.red),
+        ),
+        child: const Icon(Icons.delete));
+  }
+
+  TextEditingController travelNameView = TextEditingController();
+  static String travelName = '';
+  final formKey = GlobalKey<FormState>();
+
+  var select_start_date = '여행 시작일';
+  var select_end_date = '여행 종료일';
+  DateTime start_date = DateTime.now();
+  DateTime end_date = DateTime.now();
+
+  Widget deleteWordDialog(int id) {
+    return AlertDialog(
+      title: const Text("이 여행을 삭제하시겠습니까?"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _databaseService.deleteTravel(id).then(
+                        (result) {
+                      if (result) {
+                        Navigator.of(context).pop();
+                        setState(() {
+                          _travelList = _databaseService.selectTravels();
+                        });
+                      } else {
+                        print("delete error");
+                      }
+                    },
+                  );
+                },
+                child: const Text("예"),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("아니오"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void addTravelBottomSheet() {
+    double height = MediaQuery.of(context).size.height * 0.8;
+
+    showModalBottomSheet(
+        context: context,
+        // backgroundColor: Colors.transparent,
+        builder: (context) {
+          return Container(
+            height: height,
+            decoration: const BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20),
+              ),
+            ),
+            child:
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ScrollConfiguration(
+                behavior: MyBehavior(),
+                child: Column (
+
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      controller: travelNameView,
+                      style: const TextStyle(
+                          letterSpacing: 2,
+                          decorationThickness: 0
+                      ),
+                      cursorColor: Colors.redAccent[100],
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                            icon: Icon(Icons.close),
+                            // focusColor: Colors.redAccent[100],
+                            onPressed: () {
+                              travelNameView.clear();
+                            }),
+                        contentPadding: EdgeInsets.all(10),
+                        border: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: Colors.redAccent[100]!),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: Colors.redAccent[100]!),
+                        ),
+                        hintText: '필수 입력',
+                      ),
+                      onChanged: (String str) {
+                        setState(() => travelName = str);
+                        debugPrint(travelName);
+                      },
+                      onSaved: (val) {
+                        travelName = val!;
+                      },
+                      validator: (val) {
+                        if(val!.isEmpty) {
+                          return '아무것도 입력되지 않았습니다.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    TravelPeriod(),
+                    const SizedBox(height: 15),
+                    TravelPeopleNumber(),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {
+                        _databaseService
+                            .insertTravel(Travel(
+                            id: currentCount + 1,
+                            name: travelNameView.text))
+                            .then(
+                              (result) {
+                            if (result) {
+                              Navigator.of(context).pop();
+                              setState(() {
+                                _travelList = _databaseService.selectTravels();
+                              });
+                            } else {
+                              print("insert error");
+                            }
+                          },
+                        );
+                      },
+                      child: const Text("생성"),
+                    ),
+                  ],
+                )
+                ),
+              ),
+            );
+        }
+    );
+  }
+
 }
